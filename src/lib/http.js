348caa79b175afa04efa89fc2c890b2da16986e1
cliff <cliff.wong@ehlacademy.org>
2023-02-12 10:49:38 +0800

    token in env

--- src/lib/http.js
+++ src/lib/http.js
@@ -1,6 +1,6 @@
 import qs from 'qs'
-const VITE_API_BASE = 'http://localhost:1337'
-const VITE_STRAPI_TOKEN = '2d9df0db38eddf3b3814af6bf8d47f15af7a00a64120b45200af42f0a8e01338840c2ae3d7c35b1fc76268d1be7d77fe546e9fe53b4fb3df5301a0621b677249590f0fa3dd84d5952564a300469a4b47287781c7d3e019e75d2f4655f5600412efdf61e2a566e5cec0223a08f4ffbb0faa1b8a6bae215975d7036f1dcbbee2a5'
+const VITE_API_BASE = import.meta.env.VITE_API_BASE
+const VITE_STRAPI_TOKEN = import.meta.env.VITE_STRAPI_TOKEN
 const onRes = async (res) => {
 	return await res.json()
 }
