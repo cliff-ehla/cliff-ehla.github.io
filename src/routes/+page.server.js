5392d857d69bf88f333df1711343f6a48c440af5
cliff <cliff.wong@ehlacademy.org>
2023-03-01 18:16:11 +0800

    home page

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,37 @@
-import {redirect} from "@sveltejs/kit";
-export const load = ({params}) => {
-	throw redirect(302, '/events/category/be-our-guess/1')
+import http from "$lib/http";
+export const load = async ({fetch, params}) => {
+	const [
+		{data: events},
+		{data: posts}
+	] = await Promise.all([
+		http.get(fetch, '/events', {
+			sort: 'start_date:desc',
+			populate: {
+				author: true,
+				event_categories: true,
+				image: true
+			},
+			pagination: {
+				page: 1,
+				pageSize: 5
+			}
+		}),
+		http.get(fetch, '/posts', {
+			sort: 'createdAt:desc',
+			populate: {
+				author: true,
+				categories: true,
+				tags: true,
+				image: true
+			},
+			pagination: {
+				page: 1,
+				pageSize: 5
+			}
+		})
+	])
+	return {
+		events,
+		posts
+	}
 }
---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let data
+	import EventPreview from "$lib/EventPreview.svelte";
+	import PostPreview from "../lib/PostPreview.svelte";
+	const {events, posts} = data
+</script>
+
+<div class="container">
+	<h2 class="text-2xl px-4">近期活動</h2>
+	<div class="mx-auto p-4 max-w-screen-lg">
+		{#each events as event}
+			<EventPreview {event}/>
+		{/each}
+	</div>
+	<a class="mx-2 px-4 py-1 rounded text-blue-500 hover:bg-blue-50 border-current block border text-center"
+	   href="/events/category/all/1">查看全部活動</a>
+	<h2 class="text-2xl px-4 mt-4">近期文章</h2>
+	<div class="mx-auto p-4 max-w-screen-lg">
+		{#each posts as post}
+			<PostPreview {post}/>
+		{/each}
+	</div>
+	<a class="mx-2 px-4 py-1 rounded text-blue-500 hover:bg-blue-50 border-current block border text-center"
+	   href="/blog/category/all/1">查看全部文章</a>
+</div>
