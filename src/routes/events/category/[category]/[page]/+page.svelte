adee668e290b22c52b6cfbdbae9e6cf473f93689
Wong Hoi Ning <cliff.wong@ehlacademy.org>
2023-12-16 19:23:59 +0800

    fix paginaiton

--- src/routes/events/category/[category]/[page]/+page.svelte
+++ src/routes/events/category/[category]/[page]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
 	export let data
 	import EventPreview from "$lib/EventPreview.svelte";
+	import {page} from "$app/stores";
 	$: events = data.data
 	$: pagination = data.meta.pagination
 </script>
@@ -15,7 +16,7 @@
 	<div class="container flex flex-wrap justify-center">
 		{#each Array.from({length: pagination.pageCount}, (v, i) => i) as i}
 			<a class="text-xs flex justify-center items-center w-8 h-8 rounded-full bg-gray-100 border border-gray-300 m-1"
-			   href="/blog/category/{$page.params.category}/{i+1}">{i + 1}</a>
+			   href="/events/category/{$page.params.category}/{i+1}">{i + 1}</a>
 		{/each}
 	</div>
 </div>
